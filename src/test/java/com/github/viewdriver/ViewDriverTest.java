466db1c888426d17116add766ed73469bbbd1099
muyihuan <2711014301@qq.com>
2023-02-14 16:24:38 +0800

    单元测试准备

---
+++ src/test/java/com/github/viewdriver/ViewDriverTest.java
@@ -0,0 +1,16 @@
+package com.github.viewdriver;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 单元测试
+ *
+ * @author yanghuan
+ */
+class ViewDriverTest {
+
+    @Test
+    void test() {
+        System.out.println("hello");
+    }
+}
