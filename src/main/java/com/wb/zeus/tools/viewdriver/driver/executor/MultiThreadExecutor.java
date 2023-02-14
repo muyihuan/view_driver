658ca620f457bdfa79fc7bbeedfb7064929b0342
muyihuan <2711014301@qq.com>
2023-02-14 15:06:25 +0800

    补充readme

--- src/main/java/com/wb/zeus/tools/viewdriver/driver/executor/MultiThreadExecutor.java
+++ src/main/java/com/wb/zeus/tools/viewdriver/driver/executor/MultiThreadExecutor.java
@@ -2,14 +2,19 @@ package com.wb.zeus.tools.viewdriver.driver.executor;
 
 import com.wb.zeus.tools.viewdriver.driver.Config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
- * 多线程任务执行器.
+ * 并行任务执行器.
  *
  * @author yanghuan
  */
 public class MultiThreadExecutor implements Executor {
+
+    private List<ThreadPoolExecutor> threadPoolExecutorList = new ArrayList<>();
 
     private final Config config;
 
@@ -20,6 +25,13 @@ public class MultiThreadExecutor implements Executor {
      */
     public MultiThreadExecutor(String threadName, Config config) {
         this.config = config;
+
+        threadPoolExecutorList.add(null);
+        threadPoolExecutorList.add(null);
+        threadPoolExecutorList.add(null);
+        threadPoolExecutorList.add(null);
+        threadPoolExecutorList.add(null);
+        threadPoolExecutorList.add(null);
     }
 
     @Override
